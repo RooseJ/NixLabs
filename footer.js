115f8878cc7067d07d8bd466bb11ab7cd8a3f373
RooseJ <ahuose@gmail.com>
2023-02-14 21:17:24 -0600

    frontend changes

---
+++ footer.js
@@ -0,0 +1,41 @@
+document.write(`
+<footer id="footer" data-aos="fade-up" data-aos-easing="ease-in-out" data-aos-duration="100">
+    <div class="footer-top">
+        <div class="container">
+            <div class="row">
+
+                <div class="col-lg-4 col-md-6 footer-links">
+                    <h4>Useful Links</h4>
+                    <ul>
+                    <li><i class="bx bx-chevron-right"></i> <a href="#">Home</a></li>
+                    <li><i class="bx bx-chevron-right"></i> <a href="#">About us</a></li>
+                    <li><i class="bx bx-chevron-right"></i> <a href="#">Log in/Sign up</a></li>
+                    </ul>
+                </div>
+
+                <div class="col-lg-8 col-md-6 footer-info">
+                    <h3>About Pheonix Labs</h3>
+                    <p>Cras fermentum odio eu feugiat lide par naso tierra. Justo eget nada terra videa magna derita valies darta donna mare fermentum iaculis eu non diam phasellus.</p>
+                    <div class="social-links mt-3">
+                        <a href="" class="https://github.com/roosej"><i class="bx bxl-github"></i></a>
+                        <a href="https://www.linkedin.com/in/roosevelt-eki/" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+    <div class="container">
+      <div class="copyright">
+        <strong><span>Pheonix Labs</span></strong>. All Rights Reserved
+      </div>
+      <div class="credits">
+        <!-- All the links in the footer should remain intact. -->
+        <!-- You can delete the links only if you purchased the pro version. -->
+        <!-- Licensing information: https://bootstrapmade.com/license/ -->
+        <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/free-bootstrap-template-corporate-moderna/ -->
+        Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+      </div>
+    </div>
+</footer>`);
